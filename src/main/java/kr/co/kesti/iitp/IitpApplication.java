d49c0005529acea128d90c8d0860052c457eb8f2
jayur830 <jayur830@naver.com>
2021-08-12 14:40:41 +0900

    first commit

---
+++ src/main/java/kr/co/kesti/iitp/IitpApplication.java
@@ -0,0 +1,18 @@
+package kr.co.kesti.iitp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class IitpApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(IitpApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(IitpApplication.class, args);
+    }
+}
---
+++ src/test/java/kr/co/kesti/iitp/IitpApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kesti.iitp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IitpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
