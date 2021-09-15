3fdbdcf4933c1f4ab2d249d65136aa6b7f3dd98a
jayur830 <jayur830@naver.com>
2021-09-15 14:44:38 +0900

    divided components

---
+++ src/components/side_header/items/Items.vue
@@ -0,0 +1,65 @@
+<template>
+    <td id="items" rowspan="2">
+        <table>
+            <thead>
+                <tr>
+                    <th class="opt-head">데이터 항목</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="opt-body">
+                        <ul class="scroll no-scrollbar">
+                            <li :key="i" v-for="(item, i) in items">
+                                <input type="button" :class="selectedItems.indexOf(item) != -1 ? 'on' : ''" :value="item.label" @click.stop="setSelectedItem(item)" />
+                            </li>
+                        </ul>
+                        <div>
+                            <div @click.stop="addX">X</div>
+                            <div @click.stop="addY">Y</div>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </td>
+</template>
+
+<script>
+    import { mapState, mapActions } from "vuex";
+
+    export default {
+        name: "Items",
+        computed: {
+            ...mapState({
+                items: state => state.items,
+                selectedItems: state => state.selectedItems
+            })
+        },
+        methods: {
+            ...mapActions({
+                setSelectedItem: "SET_SELECTED_ITEM",
+                removeSelectedItem: "REMOVE_SELECTED_ITEM",
+                clearSelectedItem: "CLEAR_SELECTED_ITEM",
+                addXAxis: "ADD_X_AXIS",
+                addYAxis: "ADD_Y_AXIS"
+            }),
+
+            addX() {
+                this.addXAxis(this.selectedItems);
+                this.removeSelectedItem(this.selectedItems);
+                this.clearSelectedItem();
+            },
+
+            addY() {
+                this.addYAxis(this.selectedItems);
+                this.removeSelectedItem(this.selectedItems);
+                this.clearSelectedItem();
+            }
+        }
+    }
+</script>
+
+<style>
+    @import "./Items.css";
+</style>
