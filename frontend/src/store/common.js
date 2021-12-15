a67201c9f0002d31689eb2c3bb3836680fbb089b
jayur830 <jayur830@naver.com>
2021-12-15 18:48:18 +0900

    re-layout reactivity

--- frontend/src/store/common.js
+++ frontend/src/store/common.js
@@ -1,6 +1,7 @@
 export default {
     state: {
         common: {
+            windowWidth: window.innerWidth,
             resizeEventListeners: Object.freeze([]),
             alert: Object.freeze({
                 show: false,
@@ -23,6 +24,7 @@ export default {
             state.common.resizeEventListeners.forEach(func => window.removeEventListener("resize", func));
             state.common.resizeEventListeners = [];
         },
+        SET_WINDOW_WIDTH: (state, width) => state.common.windowWidth = width,
         SET_ALERT_INVISIBLE: state => state.common.alert = Object.freeze({ show: false, title: "", okButtonText: "", onOk: () => null }),
         SET_ALERT_VISIBLE: (state, alert) => state.common.alert = Object.freeze({ ...alert }),
         SET_LOADING_VISIBLE: state => state.common.loading = true,
@@ -31,6 +33,7 @@ export default {
     actions: {
         ADD_RESIZE_EVENT: (context, callback) => context.commit("ADD_RESIZE_EVENT", callback),
         CLEAR_RESIZE_EVENT: context => context.commit("CLEAR_RESIZE_EVENT"),
+        SET_WINDOW_WIDTH: (context, width) => context.commit("SET_WINDOW_WIDTH", width),
         SET_ALERT_INVISIBLE: context => context.commit("SET_ALERT_INVISIBLE"),
         SET_ALERT_VISIBLE: (context, alert) => context.commit("SET_ALERT_VISIBLE", alert),
         SET_LOADING_VISIBLE: context => context.commit("SET_LOADING_VISIBLE"),
