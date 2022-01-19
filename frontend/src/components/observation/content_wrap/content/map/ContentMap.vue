0edb44df8dd4f5113f4e83c6d9f869e8f26217f9
jayur830 <jayur830@naver.com>
2022-01-19 10:01:14 +0900

    back to before applying canvas

--- frontend/src/components/observation/content_wrap/content/map/ContentMap.vue
+++ frontend/src/components/observation/content_wrap/content/map/ContentMap.vue
@@ -1,14 +1,30 @@
 <template>
     <div id="map">
         <div class="map" v-show="selectedCategory == category" :key="i" v-for="(category, i) in Object.keys(stations)">
-            <geo-map
-                    :ref="category + '-map'"
-                    :zoom="windowWidth <= 1200 ? 7 : 8"
-                    :center="[36, 127.5]"
-                    :data="stations[category]"
-                    :marker="marker"
-                    :tooltip="tooltip"
-                    @click="station => setSelectedStation({ category, station })" />
+            <geo-map :ref="category + '-map'" :zoom="8" :center-lat="36" :center-lon="127.5">
+                <geo-marker
+                        :key="j"
+                        v-for="(station, j) in stations[category]"
+                        :lat="station.latitude"
+                        :lon="station.longitude"
+                        :pm25="station.pm25"
+                        @click="setSelectedStation({ category, station })">
+                    <marker-tooltip class="overlay">
+                        <div>
+                            <h4>{{ station.name }}</h4>
+                            <div>
+                                <div>위도: {{ (Math.round(station.latitude * 100) / 100).toFixed(2) }}</div>
+                                <div>경도: {{ (Math.round(station.longitude * 100) / 100).toFixed(2) }}</div>
+                                <div>위도: {{ Math.round(station.latitude * 100) / 100 }}</div>
+                                <div>경도: {{ Math.round(station.longitude * 100) / 100 }}</div>
+                                <div>주소: {{ station.address }}</div>
+                                <div>측정항목: {{ items[category].join(", ") }}</div>
+                                <div>기간평균 미세먼지 농도: {{ fineDust(station.pm25) }}</div>
+                            </div>
+                        </div>
+                    </marker-tooltip>
+                </geo-marker>
+            </geo-map>
             <div v-show="windowWidth <= 1200">스<br /><br />크<br /><br />롤</div>
         </div>
     </div>
@@ -37,45 +53,7 @@
                 selectedCategory: state => state.observation.selectedCategory,
                 stations: state => state.observation.stations,
                 selectedSearchOption: state => state.observation.selectedSearchOption
-            }),
-
-            marker() {
-                return {
-                    enableCompareDot: false,
-
-                    img(data) {
-                        if (data.pm25 == null) return require("../../../../../assets/img/marker_0.png");
-                        else if (data.pm25 < 16) return require("../../../../../assets/img/marker_1.png");
-                        else if (data.pm25 >= 16 && data.pm25 < 36) return require("../../../../../assets/img/marker_2.png");
-                        else if (data.pm25 >= 36 && data.pm25 < 76) return require("../../../../../assets/img/marker_3.png");
-                        else return require("../../../../../assets/img/marker_4.png");
-                    },
-                    imgStyle: {
-                        top: -37,
-                        left: -13.5
-                    },
-
-                    text(data) {
-                        return data.pm25 ? data.pm25 : "-";
-                    },
-                    textStyle: {
-                        top: -21,
-                        left: -1,
-                        fontStyle: "italic",
-                        fontSize: "10pt",
-                        fontFamily: "sans-serif",
-                        color: "white",
-                        verticalAlign: "middle",
-                        textAlign: "center"
-                    },
-
-                    pointCenter: {
-                        top: -19,
-                        left: 0,
-                        radius: 16
-                    }
-                }
-            }
+            })
         },
         watch: {
             startDatetime() {
@@ -88,10 +66,6 @@
 
             selectedCategory() {
                 if (this.stations[this.selectedCategory] == null) this.fetchStations();
-                setTimeout(() => this.$refs[this.selectedCategory + "-map"][0].invalidateSize(true), 0);
-            },
-
-            stations() {
                 setTimeout(() => this.$refs[this.selectedCategory + "-map"][0].invalidateSize(true), 0);
             }
         },
@@ -116,34 +90,19 @@
                 stationQuery.skip = true;
             },
 
-            tooltip(data) {
-                return `
-                    <div class="overlay" style="font: 14px 'NanumSquare'; background-color: white; box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, .29); max-width: 350px; border: 1px solid rgb(165, 165, 165); border-radius: 6px; padding: 10px;">
-                        <div>
-                            <h4 style="font: 16px 'NanumSquare'; font-weight: bold; margin: 0 0 10px 0; transform: rotate(0.03deg);">${data.name}</h4>
-                            <div style="margin-top: 5px;">
-                                <div style="margin-top: 2px; transform: rotate(0.03deg);">위도: ${Math.round(data.latitude * 100) / 100}</div>
-                                <div style="margin-top: 2px; transform: rotate(0.03deg);">경도: ${Math.round(data.longitude * 100) / 100}</div>
-                                <div style="margin-top: 2px; transform: rotate(0.03deg);">주소: ${data.address}</div>
-                                <div style="margin-top: 2px; transform: rotate(0.03deg);">측정항목: ${this.items[this.selectedCategory].join(", ")}</div>
-                                <div style="margin-top: 2px; transform: rotate(0.03deg);">
-                                    기간평균 미세먼지 농도: ${this.fineDust(data.pm25)}
-                                </div>
-                            </div>
-                        </div>
-                    </div>`;
-            },
-
             fineDust(pm25) {
-                if (pm25 == null) return "해당 기간 정보없음";
-                else if (pm25 < 16) return "좋음";
+                if (pm25 < 16) return "좋음";
                 else if (pm25 >= 16 && pm25 < 36) return "보통";
                 else if (pm25 >= 36 && pm25 < 76) return "나쁨";
                 else if (pm25 >= 76) return "매우나쁨";
+                else return "해당 기간 정보없음";
             }
         },
         mounted() {
-            Object.keys(this.items).forEach(key => this.items = Object.freeze({ ...this.items, [key]: this.$store.state.observation[key].items.map(obj => obj.label) }));
+            Object.keys(this.items).forEach(key => this.items = Object.freeze({
+                ...this.items,
+                [key]: this.$store.state.observation[key].items.map(obj => obj.label)
+            }));
             this.fetchStations();
         }
     }
