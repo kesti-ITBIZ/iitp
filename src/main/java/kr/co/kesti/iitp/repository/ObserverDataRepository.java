4768947632a31a62fc83939668f4c37b511369f1
jayur830 <jayur830@naver.com>
2022-01-20 21:16:33 +0900

    added endline on the jpql of *DataRepository

--- src/main/java/kr/co/kesti/iitp/repository/ObserverDataRepository.java
+++ src/main/java/kr/co/kesti/iitp/repository/ObserverDataRepository.java
@@ -10,6 +10,9 @@ import java.util.List;
 
 @Repository
 public interface ObserverDataRepository extends JpaRepository<ObserverData, ObserverDataKey> {
-    @Query("select distinct function('to_char', o.observerDataKey.dataTime, 'YYYY.MM.DD HH24:00') as datetime from ObserverData o where o.observerDataKey.dataTime is not null")
+    @Query(
+            "select distinct function('to_char', o.observerDataKey.dataTime, 'YYYY.MM.DD HH24:00') as datetime " +
+            "from ObserverData o " +
+            "where o.observerDataKey.dataTime is not null")
     List<String> findDistinctAllByOrderByDatetime();
 }
