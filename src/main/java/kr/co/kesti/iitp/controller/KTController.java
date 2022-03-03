b35b7378522cacf920b9b57386ea68432f6f418f
jayur830 <jayur830@naver.com>
2022-03-03 18:33:23 +0900

    added swagger

--- src/main/java/kr/co/kesti/iitp/controller/KTController.java
+++ src/main/java/kr/co/kesti/iitp/controller/KTController.java
@@ -1,8 +1,8 @@
 package kr.co.kesti.iitp.controller;
 
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
-//import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import kr.co.kesti.iitp.service.KTService;
 import kr.co.kesti.iitp.vo.RequestDataVO;
 import lombok.RequiredArgsConstructor;
@@ -12,47 +12,47 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
-//@Api(tags = "KT API")
+@Api(tags = "KT API")
 @RestController
 @RequestMapping("/api/kt")
 public class KTController {
     private final KTService ktService;
 
-//    @ApiOperation(value = "키워드 기반 KT 지점 검색")
+    @ApiOperation(value = "키워드 기반 KT 지점 검색")
     @GetMapping("/searchStations")
     public ResponseEntity<?> searchStations(
-//            @ApiParam(
-//                    value = "검색 키워드",
-//                    required = true,
-//                    example = "대")
+            @ApiParam(
+                    value = "검색 키워드",
+                    required = true,
+                    example = "대")
             @RequestParam("keyword")
             final String keyword,
-//            @ApiParam(
-//                    value = "현재 페이지",
-//                    required = true,
-//                    example = "0")
+            @ApiParam(
+                    value = "현재 페이지",
+                    required = true,
+                    example = "0")
             @RequestParam("page")
             final int page,
-//            @ApiParam(
-//                    value = "한 페이지 크기",
-//                    required = true,
-//                    example = "100")
+            @ApiParam(
+                    value = "한 페이지 크기",
+                    required = true,
+                    example = "100")
             @RequestParam("size")
             final int size) {
         return ResponseEntity.ok(this.ktService.getKtStationsByKeyword(keyword, page, size));
     }
 
-//    @ApiOperation(value = "KT 관측정보 데이터 조회")
+    @ApiOperation(value = "KT 관측정보 데이터 조회")
     @PostMapping("/getData")
     public ResponseEntity<?> getData(
-//            @ApiParam(
-//                    value = "KT 관측정보 데이터 조회 API 파라미터",
-//                    required = true)
+            @ApiParam(
+                    value = "KT 관측정보 데이터 조회 API 파라미터",
+                    required = true)
             @RequestBody final RequestDataVO request) {
         return ResponseEntity.ok(this.ktService.getKtData(request));
     }
 
-//    @ApiOperation(value = "조회 가능한 KT 데이터 조회 기간 조회")
+    @ApiOperation(value = "조회 가능한 KT 데이터 조회 기간 조회")
     @GetMapping("/getAvailableDatetimes")
     public ResponseEntity<?> getAvailableDatetimes() {
         return ResponseEntity.ok(this.ktService.getKtAvailableDatetimes());
--- src/main/java/kr/co/kesti/iitp/controller/StatisticsController.java
+++ src/main/java/kr/co/kesti/iitp/controller/StatisticsController.java
@@ -1,8 +1,8 @@
 package kr.co.kesti.iitp.controller;
 
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
-//import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import kr.co.kesti.iitp.service.StatisticsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,61 +14,61 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
-//@Api(tags = "고농도일수 통계 API")
+@Api(tags = "고농도일수 통계 API")
 @RestController
 @RequestMapping("/api/statistics")
 public class StatisticsController {
     private final StatisticsService statisticsService;
 
-//    @ApiOperation(value = "지점 조회")
+    @ApiOperation(value = "지점 조회")
     @GetMapping("/stations")
     public ResponseEntity<?> getStations(
-//            @ApiParam(
-//                    value = "데이터 기관",
-//                    required = true,
-//                    example = "airkorea")
+            @ApiParam(
+                    value = "데이터 기관",
+                    required = true,
+                    example = "airkorea")
             @RequestParam("category")
             final String category,
-//            @ApiParam(
-//                    value = "지점 조회 시간 - 시작",
-//                    required = true,
-//                    example = "202110")
+            @ApiParam(
+                    value = "지점 조회 시간 - 시작",
+                    required = true,
+                    example = "202110")
             @RequestParam("startDatetime")
             final String startDatetime,
-//            @ApiParam(
-//                    value = "지점 조회 시간 - 끝",
-//                    required = true,
-//                    example = "202203")
+            @ApiParam(
+                    value = "지점 조회 시간 - 끝",
+                    required = true,
+                    example = "202203")
             @RequestParam("endDatetime")
             final String endDatetime) {
         return ResponseEntity.ok(this.statisticsService.getStations(category, startDatetime, endDatetime));
     }
 
-//    @ApiOperation(value = "고농도 일수 통계 데이터 조회")
+    @ApiOperation(value = "고농도 일수 통계 데이터 조회")
     @GetMapping("/getData")
     public ResponseEntity<?> getData(
-//            @ApiParam(
-//                    value = "데이터 기관",
-//                    required = true,
-//                    example = "kt")
+            @ApiParam(
+                    value = "데이터 기관",
+                    required = true,
+                    example = "kt")
             @RequestParam("category")
             final String category,
-//            @ApiParam(
-//                    value = "지점 조회 시간 - 시작",
-//                    required = true,
-//                    example = "201807")
+            @ApiParam(
+                    value = "지점 조회 시간 - 시작",
+                    required = true,
+                    example = "201807")
             @RequestParam("startDatetime")
             final String startDatetime,
-//            @ApiParam(
-//                    value = "지점 조회 시간 - 끝",
-//                    required = true,
-//                    example = "201912")
+            @ApiParam(
+                    value = "지점 조회 시간 - 끝",
+                    required = true,
+                    example = "201912")
             @RequestParam("endDatetime")
             final String endDatetime,
-//            @ApiParam(
-//                    value = "조회 지점명",
-//                    required = true,
-//                    example = "화곡동 양꼬치가게앞")
+            @ApiParam(
+                    value = "조회 지점명",
+                    required = true,
+                    example = "화곡동 양꼬치가게앞")
             @RequestParam("stnNm")
             final String stnNm) {
         return ResponseEntity.ok(this.statisticsService.getStatisticsData(category, startDatetime, endDatetime, stnNm));
