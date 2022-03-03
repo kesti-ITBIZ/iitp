cfb0003a4459824a6e7dbff2dc139426b54ce838
jayur830 <jayur830@naver.com>
2022-03-03 18:16:04 +0900

    c

--- src/main/java/kr/co/kesti/iitp/controller/AirkoreaController.java
+++ src/main/java/kr/co/kesti/iitp/controller/AirkoreaController.java
@@ -1,8 +1,8 @@
 package kr.co.kesti.iitp.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
+//import io.swagger.annotations.ApiParam;
 import kr.co.kesti.iitp.service.AirkoreaService;
 import kr.co.kesti.iitp.vo.RequestDataVO;
 import lombok.RequiredArgsConstructor;
@@ -12,47 +12,47 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
-@Api(tags = "환경부 API")
+//@Api(tags = "환경부 API")
 @RestController
 @RequestMapping("/api/airkorea")
 public class AirkoreaController {
     private final AirkoreaService airkoreaService;
 
-    @ApiOperation(value = "키워드 기반 환경부 지점 검색")
+//    @ApiOperation(value = "키워드 기반 환경부 지점 검색")
     @GetMapping("/searchStations")
     public ResponseEntity<?> searchStations(
-            @ApiParam(
-                    value = "검색 키워드",
-                    required = true,
-                    example = "대")
+//            @ApiParam(
+//                    value = "검색 키워드",
+//                    required = true,
+//                    example = "대")
             @RequestParam("keyword")
             final String keyword,
-            @ApiParam(
-                    value = "현재 페이지",
-                    required = true,
-                    example = "0")
+//            @ApiParam(
+//                    value = "현재 페이지",
+//                    required = true,
+//                    example = "0")
             @RequestParam("page")
             final int page,
-            @ApiParam(
-                    value = "한 페이지 크기",
-                    required = true,
-                    example = "100")
+//            @ApiParam(
+//                    value = "한 페이지 크기",
+//                    required = true,
+//                    example = "100")
             @RequestParam("size")
             final int size) {
         return ResponseEntity.ok(this.airkoreaService.getAirkoreaStationsByKeyword(keyword, page, size));
     }
 
-    @ApiOperation(value = "환경부 관측정보 데이터 조회")
+//    @ApiOperation(value = "환경부 관측정보 데이터 조회")
     @PostMapping("/getData")
     public ResponseEntity<?> getData(
-            @ApiParam(
-                    value = "환경부 관측정보 데이터 조회 API 파라미터",
-                    required = true)
+//            @ApiParam(
+//                    value = "환경부 관측정보 데이터 조회 API 파라미터",
+//                    required = true)
             @RequestBody final RequestDataVO request) {
         return ResponseEntity.ok(this.airkoreaService.getAirkoreaData(request));
     }
 
-    @ApiOperation(value = "조회 가능한 환경부 데이터 조회 기간 조회")
+//    @ApiOperation(value = "조회 가능한 환경부 데이터 조회 기간 조회")
     @GetMapping("/getAvailableDatetimes")
     public ResponseEntity<?> getAvailableDatetimes() {
         return ResponseEntity.ok(this.airkoreaService.getAirkoreaAvailableDatetimes());
--- src/main/java/kr/co/kesti/iitp/controller/VerificationController.java
+++ src/main/java/kr/co/kesti/iitp/controller/VerificationController.java
@@ -1,8 +1,8 @@
 package kr.co.kesti.iitp.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
+//import io.swagger.annotations.ApiParam;
 import kr.co.kesti.iitp.service.VerificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,57 +15,57 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
-@Api(tags = "비교분석 API")
+//@Api(tags = "비교분석 API")
 @RestController
 @RequestMapping("/api/verification")
 public class VerificationController {
     private final VerificationService verificationService;
 
-    @ApiOperation(value = "비교분석 데이터 조회")
+//    @ApiOperation(value = "비교분석 데이터 조회")
     @GetMapping("/getData")
     public ResponseEntity<?> getData(
-            @ApiParam(
-                    value = "기준지점의 데이터 제공 기관",
-                    required = true,
-                    example = "airkorea")
+//            @ApiParam(
+//                    value = "기준지점의 데이터 제공 기관",
+//                    required = true,
+//                    example = "airkorea")
             @RequestParam("standard")
             final String standard,
-            @ApiParam(
-                    value = "비교지점의 데이터 제공 기관",
-                    required = true,
-                    example = "sDoT")
+//            @ApiParam(
+//                    value = "비교지점의 데이터 제공 기관",
+//                    required = true,
+//                    example = "sDoT")
             @RequestParam("compare")
             final String compare,
-            @ApiParam(
-                    value = "데이터 조회 시간 - 시작",
-                    required = true,
-                    example = "2021-11-12 00:00")
+//            @ApiParam(
+//                    value = "데이터 조회 시간 - 시작",
+//                    required = true,
+//                    example = "2021-11-12 00:00")
             @RequestParam("startDatetime")
             final String startDatetime,
-            @ApiParam(
-                    value = "데이터 조회 시간 - 끝",
-                    required = true,
-                    example = "2021-11-21 23:59")
+//            @ApiParam(
+//                    value = "데이터 조회 시간 - 끝",
+//                    required = true,
+//                    example = "2021-11-21 23:59")
             @RequestParam("endDatetime")
             final String endDatetime,
             @Nullable
-            @ApiParam(value = "기준지점 ID")
+//            @ApiParam(value = "기준지점 ID")
             @RequestParam("stdStnId")
             final String stdStnId,
             @Nullable
-            @ApiParam(
-                    value = "기준지점명",
-                    example = "강남구")
+//            @ApiParam(
+//                    value = "기준지점명",
+//                    example = "강남구")
             @RequestParam("stdStnNm")
             final String stdStnNm,
             @Nullable
-            @ApiParam(
-                    value = "비교지점 ID",
-                    example = "OC3CL200023")
+//            @ApiParam(
+//                    value = "비교지점 ID",
+//                    example = "OC3CL200023")
             @RequestParam("compStnId")
             final String compStnId,
             @Nullable
-            @ApiParam(value = "비교지점명")
+//            @ApiParam(value = "비교지점명")
             @RequestParam("compStnNm")
             final String compStnNm) {
         return ResponseEntity.ok(this.verificationService.getVerificationData(standard, compare, startDatetime, endDatetime, stdStnId, stdStnNm, compStnId, compStnNm));
